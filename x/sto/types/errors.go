7c3545c4af6e9ab11b40a13aca6d300d51418a13
Jin Park <jinpk@users.noreply.github.com>
2023-08-02 01:49:45 +0900

    feat: fund

--- x/sto/types/errors.go
+++ x/sto/types/errors.go
@@ -11,5 +11,6 @@ var (
 	ErrFailedStoFunding         = sdkerrors.Register(ModuleName, 1, "sto funding 입금 실패")
 	ErrInvalidShareTotal        = sdkerrors.Register(ModuleName, 2, "share total은 100이여야 합니다.")
 	ErrInsufiendOrganazierShare = sdkerrors.Register(ModuleName, 3, "초기 펀드 잔고가 부족합니다.")
+	ErrAllFunded                = sdkerrors.Register(ModuleName, 4, "fund 완료 sto")
 	ErrSample                   = sdkerrors.Register(ModuleName, 1100, "sample error")
 )
