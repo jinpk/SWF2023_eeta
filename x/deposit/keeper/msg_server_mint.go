5e91f83e77c536b4f522134e92b45724680daa78
Jin Park <jinpk@users.noreply.github.com>
2023-08-01 09:15:22 +0900

    feat: Add deposit auth logic

--- x/deposit/keeper/msg_server_mint.go
+++ x/deposit/keeper/msg_server_mint.go
@@ -2,25 +2,26 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
 	"eeta/x/deposit/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) Mint(goCtx context.Context, msg *types.MsgMint) (*types.MsgMintResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
-
 	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(msg.Coin)); err != nil {
 		return nil, err
 	}
 
-	k.Logger(ctx).Info(types.ModuleName)
-	k.Logger(ctx).Info(msg.ReceipientAddress)
-	k.Logger(ctx).Info(sdk.NewCoins(msg.Coin).String())
+	params := k.GetParams(ctx)
+	minterAddres := params.GetMinterAddress()
+	if !strings.EqualFold(minterAddres, msg.Sender) {
+		return nil, errors.Wrap("unauthorized sender address for minter")
+	}
 
 	if address, err := sdk.AccAddressFromBech32(msg.ReceipientAddress); err != nil {
 		return nil, err
